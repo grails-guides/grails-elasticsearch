00eeace3aaadfe68d02a78f1bf69ca8dc72a6886
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-09 11:58:46 +0100

    rename cluster.name=elasticsearch_puneet to cluster.name=elasticsearch

--- complete/src/main/docker/startElasticSearch.sh
+++ complete/src/main/docker/startElasticSearch.sh
@@ -4,5 +4,5 @@ set -x
 
 eval $(docker-machine env default)
 
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e ES_JAVA_OPTS="-Xms512m -Xmx512m" -e "http.cors.enabled=true" -e "http.cors.allow-origin=/https?:\/\/192.168.99.100(:[0-9]+)?/" -e "http.cors.allow-headers=Authorization,X-Requested-With,Content-Length,Content-Type" -e "http.cors.allow-credentials=true" -e "cluster.name=elasticsearch_puneet" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:5.4.3
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e ES_JAVA_OPTS="-Xms512m -Xmx512m" -e "http.cors.enabled=true" -e "http.cors.allow-origin=/https?:\/\/192.168.99.100(:[0-9]+)?/" -e "http.cors.allow-headers=Authorization,X-Requested-With,Content-Length,Content-Type" -e "http.cors.allow-credentials=true" -e "cluster.name=elasticsearch" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:5.4.3
 
